cb324a2ce1e8e915baa47e2e86fa9acde4e04b55
zishankhan-dot <56768232+zishankhan-dot@users.noreply.github.com>
2025-03-09 02:20:56 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,5 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "$sample-storage"
-    container_name = "$sample-container"
-    key = "terraform.tfstate"
+    path = "./terraform.tfstate"
   }
 }
